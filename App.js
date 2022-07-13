b4f37b52a1ea898da2a9cc50d83b6406abd02623
RdsMachado1181 <104657949+RdsMachado1181@users.noreply.github.com>
2022-07-13 17:56:25 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header  >
+      </header>
+      <main className='conteudo'> 
+
+<div className='telefone-body'>
+    <div className='body-interno'>
+
+    </div>
+</div>
+ 
+
+      </main>
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
